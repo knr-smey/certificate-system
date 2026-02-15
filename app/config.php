96b5b926253fa35ca8711a6e7f0df7b2f8f6baf1
NalenSrin123 <srinnalen@gmail.com>
2026-02-15 11:28:22 +0700

    Add teacher

--- app/config.php
+++ app/config.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 // ⚠️ Update these on InfinityFree
 defined('DB_HOST') || define('DB_HOST', 'localhost');
-defined('DB_NAME') || define('DB_NAME', 'php_etec_certificate');
-defined('DB_USER') || define('DB_USER', 'root');
-defined('DB_PASS') || define('DB_PASS', '');
+defined('DB_NAME') || define('DB_NAME', 'your_db_name');
+defined('DB_USER') || define('DB_USER', 'your_db_user');
+defined('DB_PASS') || define('DB_PASS', 'your_db_password');
 defined('DB_CHARSET') || define('DB_CHARSET', 'utf8mb4');
