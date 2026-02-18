ab36da54be4a08954a11f28608d79939363d5bc0
Ny <thoeunthonny4@gmail.com>
2026-02-18 11:37:05 +0700

    update filter students by teacher

--- app/config.example.php
+++ app/config.example.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 defined('DB_HOST') || define('DB_HOST', 'localhost');
 defined('DB_PORT') || define('DB_PORT', 3306);
-defined('DB_NAME') || define('DB_NAME', 'your_db_name');
-defined('DB_USER') || define('DB_USER', 'your_user_name');
-defined('DB_PASS') || define('DB_PASS', 'your_password');
+defined('DB_NAME') || define('DB_NAME', 'db_certificate_etec');
+defined('DB_USER') || define('DB_USER', 'root');
+defined('DB_PASS') || define('DB_PASS', '');
 defined('DB_CHARSET') || define('DB_CHARSET', 'utf8mb4');
